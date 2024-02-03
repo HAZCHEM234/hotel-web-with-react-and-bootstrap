054b51e3efbdd55121077a4e56cc302e73de69ce
HAZCHEM234 <149445613+HAZCHEM234@users.noreply.github.com>
2024-02-04 09:15:52 +1300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,23 +1,33 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Hero from './components/Hero';
+import Welcome from './components/Welcome';
+import Accommodations from './components/Accommodation';
+import FamilyS from './components/FamilyS';
+import BedBath from './components/BedBath';
+import Promo from './components/Promo';
+import Press from './components/Press';
+import Bottom from './components/Bottom';
 
 function App() {
+  const containerStyle = {
+    margin: '0', // Remove default margin
+    padding: '0', // Remove default padding
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="container-fluid" style={containerStyle}>
+        <Hero />
+        <Welcome />
+        <Accommodations />
+        <FamilyS />
+        <BedBath />
+        <Promo />
+        <Press />
+        <Bottom />
+      </div>
     </div>
   );
 }
